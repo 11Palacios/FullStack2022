16400e26276fe2f3fd0d7324037ea3211426fac9
Jose Luis Palacios <hola@jlpalacios.es>
2023-01-27 12:18:45 +0100

    1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const add = (type) => {
+    switch (type) {
+      case 'good':
+        setGood(good+1)
+        break;
+      case 'neutral':
+        setNeutral(neutral+1)
+        break;
+      case 'bad':
+        setBad(bad+1)
+        break;
+      default:
+        break;
+    }
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => add('good')}>good</button>
+      <button onClick={() => add('neutral')}>neutral</button>
+      <button onClick={() => add('bad')}>bad</button>
+      <h1>statistic</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
