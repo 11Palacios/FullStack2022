51d14e28c8492a1cb4a5f5bce065449e98e9afde
Jose Luis Palacios <hola@jlpalacios.es>
2023-01-27 12:53:18 +0100

    1.10

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
 const Statistics = ({good, neutral, bad, all, average, positive}) => {
+    if(all > 0){
+        
+    }
     return (
         <div>
             <h1>statistic</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {average}</p>
-            <p>positive {positive} %</p>
+            {all > 0 
+            ? 
+            <>
+                <p>good {good}</p>
+                <p>neutral {neutral}</p>
+                <p>bad {bad}</p>
+                <p>all {all}</p>
+                <p>average {average}</p>
+                <p>positive {positive} %</p>
+            </>
+            :
+            <>
+            <p>No Feedback given</p>
+            </>}
         </div>
     );
 }
