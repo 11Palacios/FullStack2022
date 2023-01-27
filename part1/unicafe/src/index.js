1e03eec8497e322abdba8c4247372bfe3ecb5e9d
Jose Luis Palacios <hola@jlpalacios.es>
2023-01-27 12:41:18 +0100

    1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -22,6 +22,10 @@ const App = () => {
     }
   }
 
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good * 100) / all;
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -32,6 +36,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
