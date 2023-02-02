66329172651c8730d4c84254826c381222cc20cd
Jose Luis Palacios <hola@jlpalacios.es>
2023-02-02 20:29:10 +0100

    3.3

--- back-main-app/index.js
+++ back-main-app/index.js
@@ -66,8 +66,7 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
-
-  response.json(note)
+  
 })
 
 app.delete('/api/notes/:id', (request, response) => {
