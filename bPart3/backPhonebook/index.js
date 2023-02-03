b262143080c04ff1eb6cd54c0018ce979382f362
Jose Luis Palacios <hola@jlpalacios.es>
2023-02-03 12:10:54 +0100

    3.9

--- bPart3/backPhonebook/index.js
+++ bPart3/backPhonebook/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content] - :response-time ms :date[web]'))
+app.use(cors())
 
 let persons = [
   {
