9579bb5cb1a06968b6e4b5bf89e60f077188da7e
Jose Luis Palacios <hola@jlpalacios.es>
2023-02-03 12:41:01 +0100

    3.11b

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://phonebook-jfs0.onrender.com/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
