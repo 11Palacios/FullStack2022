6d92b03ed2b7349533bde1e2d686c84cb513e2e5
Jose Luis Palacios <hola@jlpalacios.es>
2023-02-07 20:27:47 +0100

    updated main app

--- back-main-app/index.js
+++ back-main-app/index.js
@@ -27,12 +27,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
 
   const note = new Note({
     content: body.content,
@@ -40,9 +36,12 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+  .then(savedNote => savedNote.toJSON())  
+  .then(savedAndFormattedNote => {      
+    response.json(savedAndFormattedNote)    
+  }) 
+  .catch(error => next(error))
 })
 
 app.get('/api/notes', (request, response) => {
@@ -93,7 +92,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error : error.message })
+  }
 
   next(error)
 }
