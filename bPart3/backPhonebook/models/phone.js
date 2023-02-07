00745e7e8afb2a18ab6b0a1dd6e58c22e5faac68
Jose Luis Palacios <hola@jlpalacios.es>
2023-02-07 20:39:02 +0100

    3.19

--- bPart3/backPhonebook/models/phone.js
+++ bPart3/backPhonebook/models/phone.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then(result => {    console.log('connected to MongoDB')  })  .catch((error) => {    console.log('error connecting to MongoDB:', error.message)  })
+  .then(result => {    
+    console.log('connected to MongoDB')  
+  })  
+  .catch((error) => {    
+    console.log('error connecting to MongoDB:', error.message)  
+  })
+
 const phoneSchema = new mongoose.Schema({
-  name: String,
-  phone: String,
+  name: { type: String, required: true, unique: true },
+  phone: { type: String, required: true, unique: true },
 })
+
+phoneSchema.plugin(uniqueValidator);
 
 phoneSchema.set('toJSON', {
   transform: (document, returnedObject) => {
