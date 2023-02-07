051c2ddf96ecf2065527eeb09c8b53700b36423b
Jose Luis Palacios <hola@jlpalacios.es>
2023-02-07 19:34:33 +0100

    3.16

--- bPart3/backPhonebook/index.js
+++ bPart3/backPhonebook/index.js
@@ -86,6 +86,18 @@ app.delete('api/persons/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
